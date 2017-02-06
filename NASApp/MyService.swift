44a10c5f2d14afb22c3f045b823af3800cb78598
bonuelc <bonuelc@gmail.com>
2017-02-06 09:40:19 -0700

    Implement TargetType requirement: path

--- NASApp/MyService.swift
+++ NASApp/MyService.swift
@@ -20,5 +20,13 @@ extension MyService: TargetType {
     var baseURL: URL {
         return URL(string: "https://api.nasa.gov")!
     }
+    
+    var path: String {
+        switch self {
+        case .curiosity: return "/mars-photos/api/v1/rovers/curiosity/photos"
+        case .opportunity: return "/mars-photos/api/v1/rovers/opportunity/photos"
+        case .spirit: return "/mars-photos/api/v1/rovers/spirit/photos"
+        }
+    }
 }
 
