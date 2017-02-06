0655a259b8976f40612987fc8cc62cedacbba7e9
bonuelc <bonuelc@gmail.com>
2017-02-06 15:48:59 -0700

    Implement enum MyService for each Mars Rover endpoint
    # Conflicts:
    #	NASApp.xcodeproj/project.pbxproj

---
+++ NASApp/MyService.swift
@@ -0,0 +1,17 @@
+//
+//  MyService.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/5/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum MyService {
+    case curiosity
+    case opportunity
+    case spirit
+}
+
