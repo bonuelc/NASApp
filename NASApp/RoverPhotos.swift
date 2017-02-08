d8fa290e1afa4ceb52b0388a152df476f8d59661
bonuelc <bonuelc@gmail.com>
2017-02-08 12:24:53 -0700

    Add class RoverPhotos
    # Conflicts:
    #	NASApp.xcodeproj/project.pbxproj

---
+++ NASApp/RoverPhotos.swift
@@ -0,0 +1,13 @@
+//
+//  RoverPhotos.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/8/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+class RoverPhotos {
+}
+
