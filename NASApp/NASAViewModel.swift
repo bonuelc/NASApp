d21ec36959de975368f2ba7933188bfbb70ac05d
bonuelc <bonuelc@gmail.com>
2017-02-08 09:53:36 -0700

    Add class NASAViewModel

---
+++ NASApp/NASAViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NASAViewModel.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/8/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+class NASAViewModel {
+}
+
