26d7b0dee83af1454abded23538e025fd104aade
bonuelc <bonuelc@gmail.com>
2017-02-16 17:52:41 -0700

    Add class PhotoCell

---
+++ NASApp/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/16/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+}
