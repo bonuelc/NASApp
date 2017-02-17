c926c054e6f34240904bad8ee6cdda1b6467e1d7
bonuelc <bonuelc@gmail.com>
2017-02-17 10:15:57 -0700

    Add imageView property to PhotoCell

--- NASApp/PhotoCell.swift
+++ NASApp/PhotoCell.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class PhotoCell: UICollectionViewCell {
     
+    // MARK: - UI
+    
+    private let imageView: UIImageView
+    
     // MARK: - Properties
     
     static var reuseIdentifier = "photoCell"
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+     
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        imageView.contentMode = .scaleAspectFit
+        
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    
+    override func layoutSubviews() {
+        contentView.addSubview(imageView)
+    }
 }
