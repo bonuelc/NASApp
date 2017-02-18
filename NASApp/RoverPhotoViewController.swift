870a5c826721f9d0e4f90ed3845d4cc775b547db
bonuelc <bonuelc@gmail.com>
2017-02-18 11:39:37 -0700

    Add photoURL property to RoverPhotoVC

--- NASApp/RoverPhotoViewController.swift
+++ NASApp/RoverPhotoViewController.swift
@@ -9,4 +9,21 @@
 import UIKit
 
 class RoverPhotoViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let photoURL: URL
+    
+    // MARK: - Initializers
+    
+    init(photoURL: URL) {
+        
+        self.photoURL = photoURL
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
