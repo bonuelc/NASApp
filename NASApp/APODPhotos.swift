92b34720030f56579f837a9ca4b25e6040e489bf
bonuelc <bonuelc@gmail.com>
2017-02-19 15:35:54 -0700

    Add class APODPhotos

---
+++ NASApp/APODPhotos.swift
@@ -0,0 +1,12 @@
+//
+//  APODPhotos.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/19/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+class APODPhotos {
+}
