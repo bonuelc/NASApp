cc8fe5f9765cac54d70bc6c0ea5a1b4964b0cb0d
bonuelc <bonuelc@gmail.com>
2017-02-21 09:21:00 -0700

    Add ScrollViewCell

---
+++ NASApp/ScrollViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ScrollViewCell.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/20/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewCell: UICollectionViewCell {
+}
