04a22c3dd70ff80d12340f1da0881b3abf71fdcf
bonuelc <bonuelc@gmail.com>
2017-02-24 08:48:14 -0700

    Add class LandingPage

---
+++ NASApp/LandingPage.swift
@@ -0,0 +1,12 @@
+//
+//  LandingPage.swift
+//  NASApp
+//
+//  Created by Christopher Bonuel on 2/24/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class LandingPage: UIView {
+}
