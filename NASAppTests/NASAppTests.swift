41c916fdd20b3306ba07d72a65f610734ea5db15
bonuelc <bonuelc@gmail.com>
2017-02-24 16:58:14 -0700

    Add nasaVM property to NASAppTests

--- NASAppTests/NASAppTests.swift
+++ NASAppTests/NASAppTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class NASAppTests: XCTestCase {
     
+    var nasaViewModel: NASAViewModel!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
