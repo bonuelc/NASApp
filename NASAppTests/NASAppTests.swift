3bc5ef2e057f95a1621d656615e4b34d522cc3cb
bonuelc <bonuelc@gmail.com>
2017-02-24 17:03:54 -0700

    Set up nasaVM

--- NASAppTests/NASAppTests.swift
+++ NASAppTests/NASAppTests.swift
@@ -16,6 +16,7 @@ class NASAppTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        nasaViewModel = NASAViewModel()
     }
     
     override func tearDown() {
